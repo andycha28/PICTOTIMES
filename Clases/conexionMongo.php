c8d7ef47725343ddb64003a894584bb994dbb9c8
Virginia Ch <andychava@gmail.com>
2016-10-10 15:46:13 -0600

    Adding Upload needed files

---
+++ Clases/conexionMongo.php
@@ -0,0 +1,94 @@
+<?php
+
+class conexionMongo{
+		function insertarRegistro($idUsuario, $nombreImagen, $url){//cambio de parametro $idUsuario por $id_Usuario
+			try{
+				require_once('Global.php');
+				$g = new G();
+				$conexion = new Mongo($g->getDireccionMongo());
+				$baseDatos = $conexion->selectDB($g->getDB());
+				$coleccion = $baseDatos->selectCollection($g->getColeccion());
+				$listadoNombreImagen = preg_split("/[\s]+/", trim($nombreImagen), NULL, PREG_SPLIT_NO_EMPTY);
+				
+				$registro = array(
+					'usuario' => $idUsuario,
+					'nombreImagen' => $listadoNombreImagen,
+					'fecha' => new MongoDate(),
+					'url' => $url
+				);
+				return $coleccion->insert($registro);
+
+			}catch(MongoConnectionException $e) {
+				die("No es posible conectarnos a la base de datos");
+			}
+			catch(MongoException $e) {
+				die('No es posible almacenar la informacion');
+			}
+		}
+
+	
+		function getConteoColeccionImagenesTotal($nombreImagen){
+			try{
+				require_once('Global.php');
+				$g = new G();
+				$conexion = new Mongo($g->getDireccionMongo());
+				$baseDatos = $conexion->selectDB($g->getDB());
+				$coleccion = $baseDatos->selectCollection($g->getColeccion());
+				$listadoNombreImagen = preg_split("/[\s]+/", trim($nombreImagen), NULL, PREG_SPLIT_NO_EMPTY);
+				if(empty($listadoNombreImagen)){
+					$cursor = $coleccion->count();
+					return $cursor;
+				}
+				$listadoNombreImagenRegex = array();
+				foreach($listadoNombreImagen as $elemento){
+					$listadoNombreImagenRegex[] = new MongoRegex("/^". $elemento ."/");
+				}
+				if(count($listadoNombreImagenRegex)==0)
+					$listadoNombreImagenRegex = "";
+				$criterioBusqueda = array("nombreImagen"=>array('$in' => $listadoNombreImagenRegex));
+				$total = $coleccion->find($criterioBusqueda)->count();
+				return $total;
+
+			}catch(MongoConnectionException $e) {
+				die("No es posible conectarnos a la base de datos");
+			}
+			catch(MongoException $e) {
+				die('No es posible buscar la informacion');
+			}
+		}
+
+	function buscarRegistro($nombreImagen, $numSeccion)
+		{
+			try{
+				require_once('Global.php');
+				$g = new G();
+				$conexion = new MongoClient($g->getDireccionMongo());
+				$baseDatos = $conexion->selectDB($g->getDB());
+				$coleccion = $baseDatos->selectCollection($g->getColeccion());
+				$listadoNombreImagen = preg_split("/[\s]+/", trim($nombreImagen), NULL, PREG_SPLIT_NO_EMPTY);
+				if(empty($listadoNombreImagen)){
+					$cursor = $coleccion->find()->limit(3)->skip(($numSeccion-1)*3);
+					return $cursor;
+				}
+				$listadoNombreImagenRegex = array();
+				foreach($listadoNombreImagen as $elemento){
+					$listadoNombreImagenRegex[] = new MongoRegex("/^". $elemento ."/i");
+				}
+				$criterioBusqueda = array("nombreImagen"=>array('$in' => $listadoNombreImagenRegex));
+				$cursor = $coleccion->find($criterioBusqueda)->limit(3)->skip(($numSeccion-1)*3);
+				return $cursor;
+
+			}catch(MongoConnectionException $e) {
+				die("No es posible conectarnos a la base de datos");
+			}
+			catch(MongoException $e) {
+				die('No es posible buscar la informacion');
+			}
+		}
+
+
+
+
+		
+		}		
+?>
---
+++ header.php
@@ -0,0 +1,30 @@
+<?php
+	function head($title, $texto){
+?>
+
+<html>
+	<head>
+		<title>
+			PICTOTIME
+		</title>
+			
+	</head>
+
+	<body>
+
+			<a href="index.php">Inicio</a>
+			<a href="upload.php">Subir Archivo</a>
+			<a href="search.php">Buscar</a>
+			<a href="editor.php">Editor</a>
+
+			
+		<?php
+		}
+		function foot(){
+		?>
+
+	</body>
+	</html>
+		<?php
+		}
+		?>
