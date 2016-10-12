fc83c7eae30fc2673c927917c6c575e552e0b503
Virginia Ch <andychava@gmail.com>
2016-10-12 01:07:27 -0600

    adding testing upload

--- Clases/testUploadAction.php
+++ Clases/testUploadAction.php
@@ -1,13 +1,13 @@
 <?php
 require_once('Global.php');
 require_once('uploadAction.php');
-
 class testUploadAction extends PHPUnit_Framework_TestCase
 {
 	public function setUp(){}
 	public function tearDown(){}
-
-	//** Prueba unitaria, para la verificación de parametros **//
+	/**
+	 * Prueba unitaria, para la verificación de parametros 
+	 */
 	public function testCheckParamsOK(){
 		$testPT = new Pictotimefile();
 		$nombrePublicado = "namePublicado";
@@ -17,8 +17,9 @@ class testUploadAction extends PHPUnit_Framework_TestCase
 		$strRespuesta = "";
 		$this->assertTrue($testPT->check_params($nombrePublicado, $tamanhoArchivo, $isset, $tipoArchivo, $strRespuesta)==true);
 	}
-
-	//** Prueba unitaria, para el fallo en los parametros **//
+	/** 
+	 * Prueba unitaria, para el fallo en los parametros 
+	 */
 	public function testCheckParamsFAIL(){
                 $testPT = new Pictotimefile();
                 $nombrePublicado = "";
@@ -26,17 +27,21 @@ class testUploadAction extends PHPUnit_Framework_TestCase
                 $isset = false;
                 $tipoArchivo = "jpg";
                 $strRespuesta = "";
-                $this->assertTrue($testPT->check_params($nombrePublicado, $tamanhoArchivo, $isset, $tipoArchivo, $strRespuesta)==false);
+                $this->assertEquals($testPT->check_params($nombrePublicado, $tamanhoArchivo, $isset, $tipoArchivo, $strRespuesta),false);
         }
-
-	//** Prueba unitaria, para subir el archivo al ftp, valida la conexión al ftp **//
+	/** 
+	 * Prueba unitaria, para subir el archivo al ftp, valida la conexión al ftp 
+	 */
         public function testConnectFtpOK(){
 		$testPT = new Pictotimefile();
 		$this->assertTrue($testPT->connect_ftp($tmp)==true);
 	}
-
-
-
-
+	/** 
+	 * Prueba unitaria, comprobando un nombre vacio para el archcivo
+	 */
+	public function testNombreVacioOK(){
+		$nombrePublicado = "";
+		$this->assertSame($nombrePublicado,"");
+	}
 }
 ?>
